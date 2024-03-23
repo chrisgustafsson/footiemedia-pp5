7a039b9f69c234e65886b0abd72b09207da0fd9a
chrisgustafsson <christoffer.gustafsson.cg@gmail.com>
2024-03-23 22:41:16 +0000

    UPDATE NavBar with icons and minor settings

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,17 +3,19 @@ import { Navbar, Container, Nav } from "react-bootstrap";
 
 
 const NavBar = () => {
-    return <Navbar expand="md" fixed="top"><Container>
+    return ( 
+    <Navbar expand="md" fixed="top"><Container>
     <Navbar.Brand>FOOTIEMEDIA</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="mr-auto">
-        <Nav.Link>Home</Nav.Link>
-        <Nav.Link>Log in</Nav.Link>
-        <Nav.Link>Sign up</Nav.Link>
+      <Nav className="ml-auto text-left">
+        <Nav.Link><i className="fa-regular fa-futbol"></i>Home</Nav.Link>
+        <Nav.Link><i className="fas fa-sign-in-alt"></i>Sign in</Nav.Link>
+        <Nav.Link><i className="fas fa-user-plus"></i>Sign up</Nav.Link>
       </Nav>
     </Navbar.Collapse></Container>
   </Navbar>
+    );
 };
 
 export default NavBar;
