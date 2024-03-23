7c5df98e7a70fa7111e78bd5f1b6090200cdc344
chrisgustafsson <christoffer.gustafsson.cg@gmail.com>
2024-03-23 23:07:14 +0000

    ADD NavBar css styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
+import styles from "../styles/NavBar.module.css";
 
 
 const NavBar = () => {
     return ( 
-    <Navbar expand="md" fixed="top"><Container>
-    <Navbar.Brand>FOOTIEMEDIA</Navbar.Brand>
+    <Navbar className={styles.NavBar} expand="md" fixed="top"><Container>
+    <Navbar.Brand className="text-white">FOOTIEMEDIA</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="ml-auto text-left">
-        <Nav.Link><i className="fa-regular fa-futbol"></i>Home</Nav.Link>
-        <Nav.Link><i className="fas fa-sign-in-alt"></i>Sign in</Nav.Link>
-        <Nav.Link><i className="fas fa-user-plus"></i>Sign up</Nav.Link>
+        <Nav.Link className="text-white"><i className="fa-regular fa-futbol"></i>Home</Nav.Link>
+        <Nav.Link className="text-white"><i className="fas fa-sign-in-alt"></i>Sign in</Nav.Link>
+        <Nav.Link className="text-white"><i className="fas fa-user-plus"></i>Sign up</Nav.Link>
       </Nav>
     </Navbar.Collapse></Container>
   </Navbar>
