c117749e072b277bc1136dfcb581ff174a6aa454
chrisgustafsson <christoffer.gustafsson.cg@gmail.com>
2024-03-24 18:04:01 +0000

    UPDATE app with routing for first pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import styles from './App.module.css';
 import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container"
+import {Route,Switch} from 'react-router-dom';
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
-        <h1>HOME</h1>
-        <h1>SIGN IN</h1>
-        <h1>SIGN UP</h1>
+        <Switch>
+          <Route exact path="/" render={() => <h1>HOME</h1>} />
+          <Route exact path="/signin" render={() => <h1>SIGN IN</h1>} />
+          <Route exact path="/signup" render={() => <h1>SIGN UP</h1>} />
+        </Switch>
       </Container>
     </div>
   );
