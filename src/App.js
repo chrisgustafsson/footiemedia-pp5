f2be6b34ca0556f88744e1251339ede9ad860765
chrisgustafsson <christoffer.gustafsson.cg@gmail.com>
2024-03-24 17:53:05 +0000

    ADD structure and css for pages to be routed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import styles from './App.module.css';
 import NavBar from "./components/NavBar";
+import Container from "react-bootstrap/Container"
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <Container className={styles.Main}>
+        <h1>HOME</h1>
+        <h1>SIGN IN</h1>
+        <h1>SIGN UP</h1>
+      </Container>
     </div>
   );
 }
