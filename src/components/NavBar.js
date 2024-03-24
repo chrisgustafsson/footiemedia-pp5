e65852e6e941adbeafbef88fe47174e8fefb9315
chrisgustafsson <christoffer.gustafsson.cg@gmail.com>
2024-03-24 18:16:32 +0000

    ADD padding to icon + navlinks for routing

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,19 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import styles from "../styles/NavBar.module.css";
+import { NavLink } from 'react-router-dom';
 
 
 const NavBar = () => {
     return ( 
     <Navbar className={styles.NavBar} expand="md" fixed="top"><Container>
-    <Navbar.Brand className="text-white">FOOTIEMEDIA</Navbar.Brand>
+    <NavLink to="/"><Navbar.Brand className="text-white">FOOTIEMEDIA</Navbar.Brand></NavLink>
     <Navbar.Toggle className="bg-white" aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="ml-auto text-left">
-        <Nav.Link className="text-white"><i className="fa-regular fa-futbol"></i>Home</Nav.Link>
-        <Nav.Link className="text-white"><i className="fas fa-sign-in-alt"></i>Sign in</Nav.Link>
-        <Nav.Link className="text-white"><i className="fas fa-user-plus"></i>Sign up</Nav.Link>
+        <NavLink to="/" className="text-white"><i className="fa-regular fa-futbol"></i>Home</NavLink>
+        <NavLink to="/signin" className="text-white"><i className="fas fa-sign-in-alt"></i>Sign in</NavLink>
+        <NavLink to="/signup" className="text-white"><i className="fas fa-user-plus"></i>Sign up</NavLink>
       </Nav>
     </Navbar.Collapse></Container>
   </Navbar>
