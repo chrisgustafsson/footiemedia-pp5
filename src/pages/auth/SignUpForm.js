1933a40f1729940a42a69666e0a34e235b5456ec
chrisgustafsson <christoffer.gustafsson.cg@gmail.com>
2024-03-24 19:10:57 +0000

    ADD basic form for sign up

---
+++ src/pages/auth/SignUpForm.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Form, Button, Col, Row, Container } from "react-bootstrap";
+import styles from "../../styles/SignInUpForm.module.css";
+
+const SignUpForm = () => {
+  return (
+    <Row>
+      <Col md={6}>
+        <Container className="p-4">
+          <h1>SIGN UP</h1>
+          <Form>
+            <Form.Group controlId="formBasicUsername">
+              <Form.Label>Username</Form.Label>
+              <Form.Control type="text" placeholder="Enter your username" />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+
+            <Button variant="success" type="submit" size="lg" block>
+              SIGN UP
+            </Button>
+          </Form>
+        </Container>
+        <Container className="mt-3">
+          <Link className="login-link" to="/signin">Already have an account? Sign in</Link>
+        </Container>
+      </Col>
+    </Row>
+  );
+};
+
+export default SignUpForm;
