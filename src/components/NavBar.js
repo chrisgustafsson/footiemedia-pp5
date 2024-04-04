27419392a3f809468984ad6ded9875c771906f25
chrisgustafsson <christoffer.gustafsson.cg@gmail.com>
2024-04-04 22:43:08 +0000

    FIX missing axios import for navbar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,7 @@ import styles from "../styles/NavBar.module.css";
 import { NavLink } from 'react-router-dom';
 import { useCurrentUser, useSetCurrentUser } from "../contexts/CurrentUserContext";
 import Avatar from "./Avatar";
+import axios from "axios";
 
 
 const NavBar = () => {
