0a0a64b124c7a9a11c3b9ab077c29756cf4b6585
chrisgustafsson <christoffer.gustafsson.cg@gmail.com>
2024-04-04 21:26:09 +0000

    UPDATE navbar to handle logged in/logged out

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, {useContext} from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from 'react-router-dom';
+import { CurrentUserContext } from "../App";
 
 
 const NavBar = () => {
+    const currentUser = useContext(CurrentUserContext);
+    const loggedIn = <>{currentUser?.username}</>
+    const loggedOut = (
+      <>
+        <NavLink to="/signin" className="text-white"><i className="fas fa-sign-in-alt"></i>Sign in</NavLink>
+        <NavLink to="/signup" className="text-white"><i className="fas fa-user-plus"></i>Sign up</NavLink>
+      </>
+    );
+
     return ( 
     <Navbar className={styles.NavBar} expand="md" fixed="top"><Container>
     <NavLink to="/"><Navbar.Brand className="text-white">FOOTIEMEDIA</Navbar.Brand></NavLink>
@@ -12,8 +22,7 @@ const NavBar = () => {
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="ml-auto text-left">
         <NavLink to="/" className="text-white"><i className="fa-regular fa-futbol"></i>Home</NavLink>
-        <NavLink to="/signin" className="text-white"><i className="fas fa-sign-in-alt"></i>Sign in</NavLink>
-        <NavLink to="/signup" className="text-white"><i className="fas fa-user-plus"></i>Sign up</NavLink>
+        {currentUser ? loggedIn : loggedOut}
       </Nav>
     </Navbar.Collapse></Container>
   </Navbar>
